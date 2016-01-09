567d0333fd21a636827d2ab11546322e16e59960
APN-Pucky <APN-Pucky@users.noreply.github.com>
2016-01-09 14:32:08 +0100

    idk

--- index.js
+++ index.js
@@ -14,26 +14,76 @@ server.listen(port, function () {
 
 app.use(express.static(__dirname + '/public'));
 
-
-
 io.on('connection', function (socket) {
-
-  socket.on('T', function (data) {
-	console.log('takeoff');
-	client.takeoff();
-  });
-
-  socket.on('B', function (data) {
-	console.log('land');
-    	//socket.emit('button2'); 
-	client.stop();
-	client.land();
-  });
-	
-  socket.on(' ',function(data) {
-	console.log('stop');
-	client.stop();
-  });
-
+  	socket.on('T', takeoff);
+  	socket.on('B', land);
+  	socket.on('W', forward); 
+  	socket.on('S', backward); 
+  	socket.on('A', left); 
+  	socket.on('D', right); 
+  	socket.on('I', up); 
+  	socket.on('K', down); 
+  	socket.on('J', rotleft); 
+  	socket.on('L', rotright); 
+  	socket.on(' ', stop); 
+  	socket.on('C', camera); 
 });
 
+
+var $cam = 0;
+var $speed = 0.1;
+
+function log(str) {
+	console.log(str);
+}
+
+function takeoff(){
+  	client.takeoff();
+  	log('takeoff');
+}
+function land() {
+	client.stop();
+	client.land();
+	log('land');
+}
+function stop() {
+	client.stop();
+	log('stop');
+}
+function camera() {
+	$cam = ($cam+3)%6;
+ 	client.config('video:video_channel', $cam);
+	log('camera');
+}
+function forward() {
+	client.front($speed);
+	log('forward');
+}
+function backward() {
+	client.back($speed);
+	log('backward');
+}
+function left() {
+	client.left($speed);
+	log('left');
+}
+function right() {
+	client.right($speed);
+	log('right');
+}
+function up() {
+	client.up($speed);
+	log('up');
+}
+function down() {
+	client.down($speed);
+	log('speed');
+}
+function rotleft() {
+	client.counterClockwise($speed);
+	log('rotleft');
+}
+function rotright() {
+	client.clockwise($speed);
+	log('rotright');
+}
