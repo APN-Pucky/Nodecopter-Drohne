f3decbbc66c798699764a4a7d43655bc874aff0d
APN-Pucky <APN-Pucky@users.noreply.github.com>
2016-02-18 16:21:09 +0100

    better render

--- webserver/public/js/nodecopter-track.js
+++ webserver/public/js/nodecopter-track.js
@@ -3,7 +3,7 @@
 (function (window, document, undefined) {
     'use strict';
     var NodecopterTrack,
-	radius =10,
+	radius = 10,
 	zoom = 100;
     //var state = []; 
     NodecopterTrack = function(div) {
@@ -14,8 +14,12 @@
     	this.curx = 0;
     	this.cury = 0;  
     	this.curyaw = 0;  
+	this.ctx.beginPath();
 	this.ctx.font = "10px Arial";
 	this.ctx.fillText("1 m",5,10);
+	this.ctx.strokeStyle = "rgb(0,0,0)";
+	this.ctx.beginPath();
+	this.ctx.
 	this.ctx.moveTo(5,15);
 	this.ctx.lineTo(5,25);
 	this.ctx.moveTo(5,20);
@@ -23,6 +27,8 @@
 	this.ctx.moveTo(zoom+5,15);
 	this.ctx.lineTo(zoom+5,25);
 	this.ctx.stroke();
+
+	this.ctx.beginPath();
 	this.ctx.moveTo(this.x,this.y);
 	//this.update(0,0);
 
@@ -53,11 +59,12 @@
     };
 
     NodecopterTrack.prototype.update = function(state) {
+	this.ctx.beginPath();
+	this.ctx.strokeStyle = "rgb(0,255,0)";
       	this.ctx.moveTo(this.x+this.curx*zoom,this.y-this.cury*zoom);
     	this.curx = state.x;
     	this.cury = state.y;  
       	this.curyaw = state.yaw;
-	this.ctx.strokeStyle = "rgb(0,255,0)";
 	this.ctx.lineTo(this.x+(this.curx*zoom),this.y-(this.cury*zoom));
 	this.ctx.stroke();
 	//this.ctx.strokeStyle = "rgb(0,0,0)";
@@ -66,7 +73,8 @@
 
      NodecopterTrack.prototype.sonar = function(sonar) {
 	//rotate
-	//this.ctx.strokeStyle = "rgb(255,0,0)";
+	this.ctx.beginPath();
+	this.ctx.strokeStyle = "rgb(0,0,255)";
        	var dist = parseFloat(sonar.split(':')[1]);
 	var yaw = this.curyaw+parseInt(sonar.split(':')[0])*90;
 	this.ctx.moveTo(this.x+(this.curx +Math.sin(yaw)*dist)*zoom+radius,this.y-(this.cury+Math.cos(yaw)*dist)*zoom);
